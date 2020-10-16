5a997f2cd9fffa17e85405aad6b3ee39d287b29e
HudaAlrai <h.alraee94@gmail.com>
2020-10-16 21:08:38 +0300

    finish register

--- controller.js
+++ controller.js
@@ -3,7 +3,17 @@ const jwt = require("jsonwebtoken");
 const { users, roles } = require("./models");
 
 const register = async (user) => {
-
+  const pendingUser = users.filter((element) => {
+    return element.email === user.email
+  })
+  if (pendingUser.length === 0) {
+    const newUser = user;
+    newUser.password = await bcrypt.hash(user.password, Number(process.env.SALT))
+    users.push(newUser)
+    return 'new user created '
+  } else {
+    return 'you have account already'
+  }
 };
 
 const login = async (user) => {
